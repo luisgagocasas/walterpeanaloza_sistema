9274176081413d84973ca8049f99ccd4ae024afa
Luis Gago <luisgago@lagc-peru.com>
2015-01-11 02:03:27 +0000

    cleardb datos config 2 conection

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 echo $url["host"]." - ".substr($url["path"], 1)." - ".$url["user"]." - ".$url["pass"];
+$db = substr($url["path"], 1);
 // class LagcConfig {
 //     //Datos del Sitio
 //     var $lagcmail = 'hola@luisgagocasas.com';
@@ -18,9 +19,12 @@ echo $url["host"]." - ".substr($url["path"], 1)." - ".$url["user"]." - ".$url["p
 //     //Plantillas
 //     var $lagctemplsite = 'default';
 // }
-$config = new LagcConfig();
+//$config = new LagcConfig();
 $con = mysql_connect($url["host"],$url["user"],$url["pass"]);
-mysql_select_db(substr($url["path"], 1),$con) or die("<center>No hay conexion.</center>");
+mysql_select_db($db,$con) or die("<center>No hay conexion.</center>");
 mysql_set_charset('utf8');
-$respconfig = mysql_query("select * from configuracion"); $bdconfig = mysql_fetch_array($respconfig);
+//$respconfig = mysql_query("select * from configuracion"); $bdconfig = mysql_fetch_array($respconfig);
+
+$cons = mysql_connect("us-cdbr-iron-east-01.cleardb.net","heroku_aafdb04701966cf","bfccb2af599d42");
+mysql_select_db("82073233",$cons) or die("<center>No hay conexion.</center>");
 ?>
